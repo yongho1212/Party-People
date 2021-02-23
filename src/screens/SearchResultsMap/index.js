3bd3eb3af5117832241102b8b99f393ddd3eb517
yongho1212 <yongho6800@gmail.com>
2021-02-23 20:32:56 +0900

    added MAP 1/3

---
+++ src/screens/SearchResultsMap/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+
+const SearchResultsMaps = () => {
+    return (
+    <View>
+        <Text> not Just a component</Text>
+    </View>
+    );
+};
+
+
+export default SearchResultsMaps ;
